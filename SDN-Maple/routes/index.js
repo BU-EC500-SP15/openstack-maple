7f8854a4e0ff3b02e4b0a54e925ce4f9e8f6149a
naghceuz <guanchenzhang@gmail.com>
2015-03-04 03:04:13 -0500

    New Picture and files

--- SDN-Maple/routes/index.js
+++ SDN-Maple/routes/index.js
@@ -11,7 +11,7 @@ router.get('/tutorials', function(req, res, next) {
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('intro', { title: 'Express' });
+  res.render('about', { title: 'Express' });
 });
 
 
